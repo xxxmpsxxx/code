9f9836f17298e32eb927acf0f0076ab50fa0fd62
Marcus Paulo Augusto <mpsmvsjrp@hotmail.com>
2020-07-30 14:46:24 -0300

    Ajustado metodo generico find, criado post e get. Implementado campos novos nas models (imagem e url)

---
+++ CallForCode/CallForCode.API/Model/JsonFind.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallForCode.Model
+{
+    public class JsonFind
+    {
+        public string business { get; set; }
+        public string field { get; set; }
+        public string value { get; set; }
+        public string limit { get; set; }
+    }
+}
